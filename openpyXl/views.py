e835f6d2d20a529821a75e9b1138ea6f5b889d5e
wcreativo <w.kreativo@gmail.com>
2020-12-26 17:22:45 -0500

    Despues de 4 itera 1 por hoja

---
+++ openpyXl/views.py
@@ -0,0 +1,45 @@
+from django.views.generic import TemplateView
+from django.http import HttpResponse
+from openpyxl import load_workbook
+from openpyxl.drawing.image import Image
+from itertools import cycle
+import math
+
+
+class Home(TemplateView):
+    template_name = 'index.html'
+
+
+def reportXLS(request):
+    wb = load_workbook(filename='templates/20200522_Fichas_(PROPUESTA).xlsx')
+    response = HttpResponse(
+        content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    )
+    response['Content-Disposition'] = 'attachment; filename=Ficha_Campo.xlsx'
+    # ws = wb.active
+    list_photos = ['media/foto_1.jpg', 'media/foto_2.jpg', 'media/foto_3.jpg', 'media/foto_4.jpg', 'media/foto_4.jpg',
+                   'media/foto_5.jpg', 'media/foto_6.jpg', 'media/foto_7.jpg', 'media/foto_8.jpg']
+    sheets_positions = ['B7', 'Y7', 'B24', 'Y24']
+
+    list_photos_positions = list(zip(cycle(sheets_positions), list_photos))
+    print(list_photos_positions)
+
+    for index, sheet_position_photo in enumerate(list_photos_positions):
+        if index < 4:
+            sheet = wb.get_sheet_by_name('Registro_fotografico')
+            img = Image(sheet_position_photo[1])
+            img.width = 350
+            img.height = 300
+            sheet.add_image(img, sheet_position_photo[0])
+        else:
+            sheet = wb.get_sheet_by_name('Registro_fotografico')
+            new_sheet = wb.copy_worksheet(sheet)
+            img = Image(sheet_position_photo[1])
+            img.width = 350
+            img.height = 300
+            new_sheet.add_image(img, sheet_position_photo[0])
+
+    wb.save(response)
+
+    return response
+
