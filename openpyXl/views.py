95030935339b8feb56b0328609574aaab5000972
wcreativo <w.kreativo@gmail.com>
2020-12-28 16:16:52 -0500

    Adding conditionals)

--- openpyXl/views.py
+++ openpyXl/views.py
@@ -21,38 +21,32 @@ def reportXLS(request):
                    'media/foto_5.jpg', 'media/foto_6.jpg', 'media/foto_7.jpg', 'media/foto_8.jpg', 'media/foto_9.jpg',
                    'media/foto_10.jpg', 'media/foto_11.jpg', 'media/foto_12.jpg', 'media/foto_13.jpg',
                    'media/foto_14.jpg', 'media/foto_15.jpg', 'media/foto_16.jpg', 'media/foto_17.jpg']
+    
     sheets_positions = ['B7', 'Y7', 'B24', 'Y24']
 
     list_photos_positions = list(zip(cycle(sheets_positions), list_photos))
 
     sheets = []
 
-    total_sheets = (len(list_photos) // 4)
+    total_sheets = (len(list_photos) // 4) - 1 if len(list_photos) % 4 == 0 else len(list_photos) // 4
 
-    for i in range(int(total_sheets)):
-        sheet = wb.get_sheet_by_name('Registro_fotografico')
-        sheets.append(wb.copy_worksheet(sheet))
+    if len(list_photos) > 4:
+        for i in range(int(total_sheets)):
+            sheet = wb.get_sheet_by_name('Registro_fotografico')
+            sheets.append(wb.copy_worksheet(sheet))
 
     for index, sheet_position_photo in enumerate(list_photos_positions):
         if index < 4:
             sheet = wb.get_sheet_by_name('Registro_fotografico')
-            img = Image(sheet_position_photo[1])
-            img.width = 350
-            img.height = 300
-            sheet.add_image(img, sheet_position_photo[0])
         elif index < 8:
             sheet = wb.get_sheet_by_name('Registro_fotografico Copy')
-            img = Image(sheet_position_photo[1])
-            img.width = 350
-            img.height = 300
-            sheet.add_image(img, sheet_position_photo[0])
         elif index >= 8:
             sheet_name = "Registro_fotografico Copy" + str((index // 4)-1)
             sheet = wb.get_sheet_by_name(sheet_name)
-            img = Image(sheet_position_photo[1])
-            img.width = 350
-            img.height = 300
-            sheet.add_image(img, sheet_position_photo[0])
+        img = Image(sheet_position_photo[1])
+        img.width = 350
+        img.height = 300
+        sheet.add_image(img, sheet_position_photo[0])
 
     wb.save(response)
 
